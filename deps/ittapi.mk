2bf306c3a06a9d6bf5ee89263ab7851e27fe05fb
Valentin Churavy <v.churavy@gmail.com>
2023-03-16 15:59:00 -0400

    fixup! Add ITTAPI source for offline

--- deps/ittapi.mk
+++ deps/ittapi.mk
@@ -7,7 +7,8 @@ $(eval $(call git-external,ittapi,ITTAPI,CMakeLists.txt,,$(SRCCACHE)))
 
 get-ittapi: $(ITTAPI_SRC_FILE)
 extract-ittapi: $(SRCCACHE)/$(ITTAPI_SRC_DIR)/source-extracted
-configure-libgit2:
-compile-libgit2:
-fastcheck-libgit2: #none
-check-libgit2:
+configure-ittapi:
+compile-ittapi:
+install-ittapi:
+fastcheck-ittapi: #none
+check-ittapi:
