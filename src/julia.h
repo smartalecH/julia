d1e4c7aea485b55381b240d2792dd3cd2ad7054d
Valentin Churavy <v.churavy@gmail.com>
2023-03-18 13:25:09 -0400

    Use ITTAPI to inform VTunes about Julia tasks

--- src/julia.h
+++ src/julia.h
@@ -77,6 +77,12 @@ typedef struct _jl_tls_states_t *jl_ptls_t;
 #include "julia_threads.h"
 #include "julia_assert.h"
 
+#if defined(USE_ITTAPI)
+#define INTEL_ITTNOTIFY_API_PRIVATE
+#include "ittapi/ittnotify.h"
+#undef INTEL_ITTNOTIFY_API_PRIVATE
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -1950,6 +1956,9 @@ typedef struct _jl_task_t {
     uint16_t reentrant_timing; // How many times we've reentered timing
     unsigned int copy_stack:31; // sizeof stack for copybuf
     unsigned int started:1;
+#if defined(USE_ITTAPI)
+    __itt_id itt_id;
+#endif
 } jl_task_t;
 
 #define JL_TASK_STATE_RUNNABLE 0
